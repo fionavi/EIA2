dcc7bb423bc09e3fe6990e45720a9a321891bf9f
Fiona <fiona.noemi.virnich@hs-furtwangen.de>
2021-04-17 13:03:15 +0200

    L02 events

--- Versuch2/L02/eventHandler.js
+++ Versuch2/L02/eventHandler.js
@@ -2,21 +2,34 @@
 var L01;
 (function (L01) {
     window.addEventListener("load", handleLoad);
+    let button1 = document.getElementById("b1");
     function handleLoad(_event) {
-        console.log("handleLoad start");
-        window.addEventListener("mousemovement", setInfoBox);
+        window.addEventListener("mousemove", setInfoBox);
         window.addEventListener("click", logInfo);
         window.addEventListener("keyup", logInfo);
-        //   document.querySelector("#infos").addEventListener("click", handleInfos);
+        document.querySelector("#b1").addEventListener("click", customEvent);
     }
     function setInfoBox(_event) {
-        console.log("setInfoBox start");
-        let span = document.createElement("span");
-        span.id = "span";
-        //span.style.position = mouseposition + offset
+        let span = document.getElementById("span");
+        let mousex = _event.clientX; // Gets Mouse X
+        let mousey = _event.clientY; // Gets Mouse Y
+        let offset = 10;
+        span.style.left = mousex + offset + "px";
+        span.style.top = mousey + offset + "px";
+        span.innerHTML = "position: " + mousex + ", " + mousey + "<br>" + _event.target;
     }
     function logInfo(_event) {
-        console.log("logInfo start");
+        console.log("type: " + _event.type);
+        console.log("target: " + _event.target);
+        console.log("current target: " + _event.currentTarget);
+        console.log("object: " + _event);
+    }
+    function customEvent(_event) {
+        console.log("button was pressed");
+        let data = "This is my data.";
+        let event = new CustomEvent("click", { bubbles: true, detail: { key: data } });
+        button1.dispatchEvent(event);
+        console.log(event + " event wurde erzeugt");
     }
 })(L01 || (L01 = {}));
 //# sourceMappingURL=eventHandler.js.map
--- Versuch2/L02/eventHandler.ts
+++ Versuch2/L02/eventHandler.ts
@@ -1,28 +1,47 @@
 namespace L01 {
 
     window.addEventListener("load", handleLoad);
+    let button1: HTMLElement = document.getElementById("b1");
 
     function handleLoad(_event: Event): void {
-        console.log("handleLoad start");
 
-        window.addEventListener("mousemovement", setInfoBox);
+        window.addEventListener("mousemove", setInfoBox);
         window.addEventListener("click", logInfo);
         window.addEventListener("keyup", logInfo);
-     //   document.querySelector("#infos").addEventListener("click", handleInfos);
-    
+        document.querySelector("#b1").addEventListener("click", customEvent);
+
     }
 
 
-    function setInfoBox(_event: Event): void {
-        console.log("setInfoBox start");
-        let span: HTMLSpanElement = document.createElement("span");      
-        span.id = "span";
-        //span.style.position = mouseposition + offset
+    function setInfoBox(_event: MouseEvent): void {
+        let span: HTMLSpanElement = document.getElementById("span");
+
+        let mousex: number = _event.clientX; // Gets Mouse X
+        let mousey: number = _event.clientY; // Gets Mouse Y
+        let offset: number = 10;
+
+
+
+        span.style.left = mousex + offset + "px";
+        span.style.top = mousey + offset + "px";
+        span.innerHTML = "position: " + mousex + ", " + mousey + "<br>" + _event.target;
     }
 
 
     function logInfo(_event: Event): void {
-        console.log("logInfo start");
+
+        console.log("type: " + _event.type);
+        console.log("target: " + _event.target);
+        console.log("current target: " + _event.currentTarget);
+        console.log("object: " + _event);
     }
 
+    function customEvent(_event: Event): void {
+        console.log("button was pressed");
+        let data: string = "This is my data.";
+        let event: CustomEvent = new CustomEvent("click", { bubbles: true, detail: { key: data } });
+        button1.dispatchEvent(event);
+        console.log(event + " event wurde erzeugt");
+    }
 }
+
